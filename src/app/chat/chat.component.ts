30942222c9e98701a7e98253215a0ea7ede95ee3
Irini Koutaki <irinikp@gmail.com>
2019-03-21 20:47:30 +0200

    fetch firestore collection

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,10 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
+import {
+  AngularFirestoreCollection,
+  AngularFirestore
+} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-chat',
@@ -6,12 +12,20 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
-  @Input() userAuth: string;
 
-  constructor() { }
+  @Input() userAuth: string;
+  messages: Observable<any[]>;
+  private msgRef: AngularFirestoreCollection<Message>;
+
+  constructor(private db: AngularFirestore) {
+    this.msgRef = db.collection<Message>('messages', ref =>
+      ref.orderBy('timestamp')
+    );
+  }
 
   ngOnInit() {
+    this.messages = this.msgRef.valueChanges();
+    this.messages.subscribe(res => console.log(res));
   }
 
 }
-
