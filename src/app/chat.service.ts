38349eda36cf0645f15e91972fcab057bbab8392
Irini Koutaki <irinikp@gmail.com>
2019-03-21 20:33:55 +0200

    create service to handle firestore functions

---
+++ src/app/chat.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Message } from './message';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+
+  messages: AngularFirestoreCollection<Message>;
+
+  constructor(private db: AngularFirestore) {
+    this.messages = db.collection<Message>('messages');
+  }
+
+  addMsg(msg) {
+    this.messages.add(msg);
+  }
+}
